d4d66e8334614603cc91b80e9b25fa074bd0ac02
= <=>
2018-12-17 14:46:11 +0100

    moving post array from post-list to app-component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'blog';
+
+
+  posts: Array<Object> = [
+    {
+      title:'Mon premier post',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti deserunt ad sit fugiat incidunt unde commodi, omnis itaque quas iste, perspiciatis consequatur neque dolorum blanditiis ullam nisi quam ex!',
+      loveIts: 0,
+      created_at: new Date()
+  
+    },
+  
+    {
+      title:'Mon deuxième message',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti deserunt ad sit fugiat incidunt unde commodi, omnis itaque quas iste, perspiciatis consequatur neque dolorum blanditiis ullam nisi quam ex!',
+      loveIts: 0,
+      created_at: new Date()
+  
+    },
+    
+  
+    {
+      title:'Enfin, un troisième article',
+      content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti deserunt ad sit fugiat incidunt unde commodi, omnis itaque quas iste, perspiciatis consequatur neque dolorum blanditiis ullam nisi quam ex!',
+      loveIts: 0,
+      created_at: new Date()
+  
+    },
+  
+    ]
+    
 }
--- src/app/post-list/post-list.component.ts
+++ src/app/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -7,33 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostListComponent implements OnInit {
 
-  posts: Array<Object> = [
-  {
-    title:'Mon premier post',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti deserunt ad sit fugiat incidunt unde commodi, omnis itaque quas iste, perspiciatis consequatur neque dolorum blanditiis ullam nisi quam ex!',
-    loveIts: 0,
-    created_at: new Date()
-
-  },
-
-  {
-    title:'Mon deuxième message',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti deserunt ad sit fugiat incidunt unde commodi, omnis itaque quas iste, perspiciatis consequatur neque dolorum blanditiis ullam nisi quam ex!',
-    loveIts: 0,
-    created_at: new Date()
-
-  },
-  
-
-  {
-    title:'Enfin, un troisième article',
-    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore corrupti deserunt ad sit fugiat incidunt unde commodi, omnis itaque quas iste, perspiciatis consequatur neque dolorum blanditiis ullam nisi quam ex!',
-    loveIts: 0,
-    created_at: new Date()
-
-  },
-
-  ]
+  @Input() posts;
 
   constructor() { }
 
